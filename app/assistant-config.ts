eb984f1278e0bd3749c7be64f2398dc791ae393a
MisterOPS <jacobseloy@gmail.com>
2025-04-18 14:49:53 +0200

    Update assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = ""; // set your assistant ID here
+export let assistantId = "asst_AQ1GGmoxkgUdGjTiEry8pt4Y"; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
